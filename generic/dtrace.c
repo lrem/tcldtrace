c899b388a3f33027865eb4b836529102e2a13bb4
lrem <lrem@b9bceb66-5125-4edc-b60c-05fa4fa970b3>
2008-07-08 16:38:29 +0000

    Starting implementation of the binding.
    git-svn-id: svn+ssh://dev.lrem.net/home/trac/svn/tcldtrace/trunk@9 b9bceb66-5125-4edc-b60c-05fa4fa970b3

---
+++ generic/dtrace.c
@@ -0,0 +1,37 @@
+#include <tcl.h>
+#include "dtrace.h"
+
+int Open (ClientData cd, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
+{
+	printf("hi!\n");
+
+	return TCL_OK;
+}
+
+int Dtrace_Init (Tcl_Interp *interp)
+{
+	Tcl_Namespace *namespace;
+
+	/* As I understood it, Tcl_InitStubs is to make the extension linkable
+	 * against any version of interpreter that supports it. (Not only 
+	 * against the binary available at compile time).
+	 */
+	if (Tcl_InitStubs(interp, "8.1", 0) == NULL 
+			|| Tcl_PkgRequire(interp, "Tcl", "8.4", 0) == NULL) 
+	{
+		return TCL_ERROR;
+	}
+
+	if (Tcl_PkgProvide(interp, "dtrace", PACKAGE_VERSION) != TCL_OK) {
+		return TCL_ERROR;
+	}
+
+	namespace = Tcl_CreateNamespace(interp, NS, NULL, NULL);
+	if(namespace == NULL)
+		return TCL_ERROR;
+
+	Tcl_CreateObjCommand(interp, NS "::open", (Tcl_ObjCmdProc *) Open,
+			(ClientData)NULL, (Tcl_CmdDeleteProc *)NULL);
+	
+	return TCL_OK;
+}
---
+++ generic/dtrace.h
@@ -0,0 +1,7 @@
+#ifndef __DTRACE_H
+#define __DTRACE_H
+
+#define PACKAGE_VERSION "1.0"
+#define NS "dtrace"
+
+#endif /* __DTRACE_H */
