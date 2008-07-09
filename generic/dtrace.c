9973c46263e7ecff86c8ce3bfc9fd81dbf05bbd9
lrem <lrem@b9bceb66-5125-4edc-b60c-05fa4fa970b3>
2008-07-09 11:14:56 +0000

    This should be enough for #9
    git-svn-id: svn+ssh://dev.lrem.net/home/trac/svn/tcldtrace/trunk@10 b9bceb66-5125-4edc-b60c-05fa4fa970b3

--- generic/dtrace.c
+++ generic/dtrace.c
@@ -1,3 +1,5 @@
+#include <dtrace.h>
+#include <libproc.h>
 #include <tcl.h>
 #include "dtrace.h"
 
@@ -11,12 +13,14 @@ int Open (ClientData cd, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
 int Dtrace_Init (Tcl_Interp *interp)
 {
 	Tcl_Namespace *namespace;
+	int major;
+	int minor;
 
 	/* As I understood it, Tcl_InitStubs is to make the extension linkable
 	 * against any version of interpreter that supports it. (Not only 
 	 * against the binary available at compile time).
 	 */
-	if (Tcl_InitStubs(interp, "8.1", 0) == NULL 
+	if (Tcl_InitStubs(interp, "8.4", 0) == NULL 
 			|| Tcl_PkgRequire(interp, "Tcl", "8.4", 0) == NULL) 
 	{
 		return TCL_ERROR;
@@ -26,12 +30,24 @@ int Dtrace_Init (Tcl_Interp *interp)
 		return TCL_ERROR;
 	}
 
+	/* This creates a warning. Solaris does not have <tclInt.h> */
 	namespace = Tcl_CreateNamespace(interp, NS, NULL, NULL);
 	if(namespace == NULL)
 		return TCL_ERROR;
 
 	Tcl_CreateObjCommand(interp, NS "::open", (Tcl_ObjCmdProc *) Open,
 			(ClientData)NULL, (Tcl_CmdDeleteProc *)NULL);
-	
+
+	Tcl_GetVersion(&major, &minor, NULL, NULL);
+	if(8 <= major && 5 <= minor)
+	{
+		/* Tcl_Export(interp, namespace, "*", 0);
+		Tcl_CreateEnsemble(interp, NS, namespace, 0); */
+		Tcl_Eval(interp, "namespace eval " NS " {\n"
+				"namespace export *\n"
+				"namespace ensemble create\n"
+				"}");
+	}
+
 	return TCL_OK;
 }
