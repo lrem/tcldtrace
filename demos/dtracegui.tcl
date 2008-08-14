9054b504cdae0600c05a284a5db0f1a821374adf
lrem <lrem@b9bceb66-5125-4edc-b60c-05fa4fa970b3>
2008-08-14 21:21:52 +0000

    Adding folds and copyright header.
    git-svn-id: svn+ssh://dev.lrem.net/home/trac/svn/tcldtrace/trunk@132 b9bceb66-5125-4edc-b60c-05fa4fa970b3

--- demos/dtracegui.tcl
+++ demos/dtracegui.tcl
@@ -1,6 +1,40 @@
 #!/opt/ActiveTcl-8.5/bin/tclsh
+
+# Copyright header {{{
+#
+# Copyright (c) 2008, Remigiusz 'lRem' Modrzejewski
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of Remigiusz Modrzejewski nor the
+#       names of his contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY Remigiusz Modrzejewski ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL Remigiusz Modrzejewski BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# }}}
+# Includes {{{
+
 package require Tk
 package require dtrace
+
+# }}}
+# Screen layout {{{
 
 image create photo logoObj -file "[file dirname [info script]]/logo.gif"
 grid [label .logo -image logoObj] -columnspan 2
@@ -25,6 +59,9 @@ grid [text .aggregations.text -width 60 -height 100 -tabs {400 right} \
 grid [labelframe .output -text "Probes"] -row 2 -column 1
 grid [text .output.text -width 60 -height 100 -state disabled]
 
+# }}}
+# Simple button actions {{{
+
 proc clearEditor {} {
     .editor.code delete 1.0 end
 }
@@ -38,6 +75,9 @@ set order 0
 proc armageddon {} {
     foreach button [grid slaves .active] {$button invoke}
 }
+
+#}}}
+# Probe enabling/disabling {{{
 
 proc enableProbe {} {
     global topId
@@ -59,6 +99,9 @@ proc disableProbe {id handle} {
     dtrace close $handle
     grid forget .active.$id
 }
+
+# }}}
+# Output handing and dtraceLoop {{{
 
 proc descCallback {probe args} {
     .output.text configure -state normal
@@ -88,5 +131,6 @@ proc dtraceLoop {handle id} {
         after 300 [list dtraceLoop $handle $id]
     }
 }
+#}}}
 
-# vim: set ts=4 sw=4 et:
+# vim: set ts=4 sw=4 et foldmethod=marker:
